04d7ecb7d74e8b895af1c9a0d08d5006dca8295e
whatevsz <hannes.koerber@gmail.com>
2013-03-05 16:20:56 +0100

    Filled setup.py.

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='autobackup',
+      version = '0.1-dev',
+      description = '',
+      long_description = open("README.md").read(),
+      license = open("LICENSE").read(),
+      author = 'Hannes Körber',
+      auther_email = 'hannes.koerber@gmail.com',
+      url = 'http://github.com/whatevsz/autobackup/',
+      packages=[]
+      )
