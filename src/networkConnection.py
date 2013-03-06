3965c349f37140c20cc794acd9c29a02c1cbd88f
whatevsz <hannes.koerber@gmail.com>
2013-03-06 16:51:32 +0100

    Changed the interface after conclusions derived from sshNetworkConnection.

--- src/networkConnection.py
+++ src/networkConnection.py
@@ -4,15 +4,21 @@ class NetworkConnection(object):
     def __init__(self, Host, port):
         raise NotImplementedError()
     
-    def connect(self, timeout):
-        """Connects to the host and raises a timeout error if <timeout> (in ms) exceeded."""
+    def connect(self, user, timeout, remoteShell):
+        """Connects to the host as <user> and starts a shell specified by <remoteShell>.
+        Raises a timeout error if <timeout> (in milliseconds) exceeded."""
         raise NotImplementedError()
     
     def disconnect(self):
         """Disconnects the connection immediately."""
         raise NotImplementedError()
     
-    def execute(self, user, command):
-        """Executes a <command> on the remote host as user <user>."""
+    def execute(self, command, timeout):
+        """Executes a <command> on the remote host as user <user>. Raises a timeout error
+        if the command did not finish after <timeout> milliseconds."""
+        raise NotImplementedError()
+    
+    def isConnected(self):
+        """Returns a bool that specifies whether the connection is established."""
         raise NotImplementedError()
         
