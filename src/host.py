3a226d4de8f0b2fc3671c9402d7d8a21f6bd50a7
whatevsz <hannes.koerber@gmail.com>
2013-03-07 17:43:47 +0100

    __eq__ and _ne__ added.
    Overloaded the equal and not equal operators as a simple comparison of
    the ips is not
    sufficent. See the comment for more datail.

--- src/host.py
+++ src/host.py
@@ -18,3 +18,14 @@ class Host(object):
         return self.__ip
     ip = property(__getIP)
     
+    # Needs to be overloaded, as a simple comparison of the ips is not sufficient.
+    # All ips in 127.*.*.* refer to the localhost, so for instance 127.0.0.1 and
+    # 127.42.13.37 are the same machine.
+    def __eq__(self, other):
+        if self.isLocalhost() and other.isLocalhost():
+            return True
+        return self.ip == other.ip
+    
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    
