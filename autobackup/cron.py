8127f1bf8c61a57a8e03ee2fdc336fca22e4cbd3
Hannes Körber <hannes.koerber@gmail.com>
2013-03-17 04:35:21 +0100

    Started implementing cron.py and testfile.

--- autobackup/cron.py
+++ autobackup/cron.py
@@ -1,11 +1,58 @@
 import datetime
 
-class Cronjob(object):
+ranges = (range(60), range(24), range(1,32), range(1,13), range(1,8), 
+          range(1900,3000)) # Creating year 3000 problem
 
+class Crontab(object):
+    """
+    Crontab format:
+    minute hour day month weekday year
+    """
     def __init__(self, schedule_string):
-        self.schedule_string = schedule_string
+        self.schedule = (0, 0, 0, 0, 0, 0)
+        self.schedule = _parse_schedule_string(schedule_string)
+        for i in range(6):  
+            self.schedule[i] = _get_values(self.schedule[i], i)
+        
+
+    def matches(self, datetime):
+        (d_year, d_month, d_day, d_hour, d_minute, _, d_weekday, _, _) = \
+            datetime.timetuple()
+        d_schedule = (d_minute, d_hour, d_day, d_month, d_weekday, d_year)
+        for i in range(6):
+            if not d_schedule[i] in self.schedule[i]:
+                return False
+        return True
 
 
     def get_latest_occurence(self):
-        return datetime.datetime.now()
+        now = datetime.datetime.now()
 
+        
+    def update(self):
+        now = datetime.datetime.now()
+        (self.year, self.month, self.day, self.hour, self.minute, _, 
+            self.weekday, _, _) = now.timetuple()
+        
+       
+    
+def _get_values(string, index):
+    if string.strip() == '*':
+        return ranges[index]
+    if ',' in string:
+        parts = [int(val) for val in string.split(',')]
+        return parts
+    if '-' in string:
+        parts = [int(val) for val in string.split('-')]
+        if len(parts) != 2:
+            Exception("Invalid value.")
+        return range(parts[0], parts[1] + 1)
+    return [int(string)]
+    
+    
+def _parse_schedule_string(schedule_string):
+    #schedules = []
+    #for schedule in schedule_string.split(';'):
+    #    schedules.append(schedule.split())
+    #return schedules
+    return schedule_string.split()[:6]
---
+++ tests/test_cron.py
@@ -0,0 +1,38 @@
+import unittest
+import datetime
+
+import cron
+
+class Tests(unittest.TestCase):
+    
+    def setUp(self):
+        self.c1 = cron.Crontab("1 10-15 5 6,7,8,11 * 2012-2015")
+
+    def test_matching(self):
+        d_in_lo    = datetime.datetime(2012,  6,  5, 10,  1)
+        d_out_lo_1 = datetime.datetime(2011,  6,  5, 10,  1)
+        d_out_lo_2 = datetime.datetime(2012,  5,  5, 10,  1)
+        d_out_lo_3 = datetime.datetime(2012,  6,  4, 10,  1)
+        d_out_lo_4 = datetime.datetime(2012,  6,  5,  9,  1)
+        d_out_lo_5 = datetime.datetime(2012,  6,  5, 10,  0)
+
+        d_in_hi    = datetime.datetime(2015, 11,  5, 15,  1)
+        d_out_hi_1 = datetime.datetime(2016, 11,  5, 15,  1)
+        d_out_hi_2 = datetime.datetime(2015, 12,  5, 15,  1)
+        d_out_hi_3 = datetime.datetime(2015, 11,  6, 15,  1)
+        d_out_hi_4 = datetime.datetime(2015, 11,  5, 16,  1)
+        d_out_hi_5 = datetime.datetime(2015, 11,  5, 15,  2)
+
+        self.assertTrue(self.c1.matches(d_in_lo))
+        self.assertFalse(self.c1.matches(d_out_lo_1))
+        self.assertFalse(self.c1.matches(d_out_lo_2))
+        self.assertFalse(self.c1.matches(d_out_lo_3))
+        self.assertFalse(self.c1.matches(d_out_lo_4))
+        self.assertFalse(self.c1.matches(d_out_lo_5))
+
+        self.assertTrue(self.c1.matches(d_in_hi))
+        self.assertFalse(self.c1.matches(d_out_hi_1))
+        self.assertFalse(self.c1.matches(d_out_hi_2))
+        self.assertFalse(self.c1.matches(d_out_hi_3))
+        self.assertFalse(self.c1.matches(d_out_hi_4))
+        self.assertFalse(self.c1.matches(d_out_hi_5))
