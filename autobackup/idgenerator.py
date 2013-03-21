5afa6ce0f2fb169f6585437073a118bee121e9d2
Hannes Körber <hannes.koerber@gmail.com>
2013-03-21 20:58:58 +0100

    Merged fdmanager.py and idgenerator.py back into networkconnection.py.

--- autobackup/idgenerator.py
+++
@@ -1,8 +0,0 @@
-import string
-import random
-
-def generate_id(size, 
-                chars=string.ascii_uppercase + 
-                string.ascii_lowercase + 
-                string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
--- autobackup/networkconnection.py
+++ autobackup/networkconnection.py
@@ -205,3 +205,38 @@ class SSHNetworkConnection(NetworkConnection):
 
     def is_connected(self):
         return self.ssh_process != None
+def generate_id(size,
+            chars=string.ascii_uppercase +
+            string.ascii_lowercase +
+            string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def unblock_file_descriptor(path):
+    file_descriptor = path.fileno()
+    file_control = fcntl.fcntl(file_descriptor, fcntl.F_GETFL)
+    fcntl.fcntl(file_descriptor, fcntl.F_SETFL, file_control | os.O_NONBLOCK)
+
+
+def read_all(path):
+    output = []
+    while True:
+        line = non_blocking_read(path)
+        if not line:
+            break
+        else:
+            output.append(line)
+    return "".join(output)
+
+
+def non_blocking_readline(path):
+    try:
+        return path.readline()
+    except IOError:
+        return ""
+
+def non_blocking_read(path):
+    try:
+        return path.read()
+    except IOError:
+        return ""
